cda7a155a805634ffde3ba53a4ab4ceb99030c1c
yuanhaojin <yuanhaojin@tencent.com>
2021-04-12 17:02:34 +0800

    error

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
-	r := gin.Default()
+	r := ""
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "hello github2",
